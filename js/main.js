545b9d486d4b52f30d9810bbb7a621b6b0f7a20b
raiweb <rafaelvargas1164@gmail.com>
2025-03-25 08:26:10 -0400

    Subir Archivos

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="typed">Una Señal Que Fortalece </i>',
+		'<i class="typed">Tu Espíritu... </i>',
+		
+		'<i class="typed">Que el Dios de la esperanza </i>',
+		'<i class="typed">los llene de toda alegría y paz </i>',
+		'<i class="typed">a ustedes que creen en él, para que </i>',
+		'<i class="typed">rebosen de esperanza por el poder </i>',
+		'<i class="typed">del Espíritu Santo. Romanos 15:13. </i>',
+        
+		'<i class="typed">Jesús le respondió: Escrito está: </i>',
+		'<i class="typed">“No solo de pan vive el hombre, </i>',
+		'<i class="typed">sino de toda palabra que sale </i>',
+		'<i class="typed">de la boca de Dios” </i>',
+        '<i class="typed">Mateo 4:4. </i>',
+        		       
+		'<i class="typed">El que oye mi palabra, </i>',
+        '<i class="typed">y cree al que me envió, tiene vida eterna </i>',
+        '<i class="typed">Juan 5:24. </i>',
+		
+		'<i class="typed">El Señor es mi fuerza y mi escudo;</i>',
+		'<i class="typed">mi corazón en él confia;</i>',
+		'<i class="typed">de él recibo ayuda</i>',
+		'<i class="typed">Mi corazón salta de alegría,</i>',
+		'<i class="typed">y con cánticos le daré gracias.</i>',
+		'<i class="typed">Salmos 28:7.</i>',
+		
+		
+		
+	],
+
+	 
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 25, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+	
+
+
+});
+
+
